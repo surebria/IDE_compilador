b5f73dec10a49a6feb9c18d6cb5a436f8142b25f
XitlaliReyes <al262973@edu.uaa.mx>
2025-02-26 21:02:12 -0600

    Contabilizador de lineas
    Linea

--- ide.py
+++ ide.py
@@ -1,8 +1,106 @@
 from PyQt6.QtWidgets import (
-    QApplication, QMainWindow, QTextEdit, QVBoxLayout, QWidget, QTabWidget, QSplitter, QMenuBar, QMenu, QFileDialog, QLabel
+    QApplication, QMainWindow, QTextEdit, QVBoxLayout, QWidget, QTabWidget, QSplitter, QMenuBar, QMenu, 
+    QFileDialog, QLabel, QPlainTextEdit, QHBoxLayout
 )
-from PyQt6.QtGui import QAction
+from PyQt6.QtGui import QAction, QColor, QPainter, QTextFormat, QFontMetrics
+from PyQt6.QtCore import QRect, Qt, QSize, pyqtSlot
 import sys
+
+class LineNumberArea(QWidget):
+    def __init__(self, editor):
+        super().__init__(editor)
+        self.editor = editor
+
+    def sizeHint(self):
+        return QSize(self.editor.line_number_area_width(), 0)
+
+    def paintEvent(self, event):
+        self.editor.lineNumberAreaPaintEvent(event)
+
+
+class CodeEditor(QPlainTextEdit):
+    def __init__(self):
+        super().__init__()
+        self.line_number_area = LineNumberArea(self)
+        
+        self.blockCountChanged.connect(self.update_line_number_area_width)
+        self.updateRequest.connect(self.update_line_number_area)
+        self.cursorPositionChanged.connect(self.highlight_current_line)
+        
+        self.update_line_number_area_width(0)
+        self.highlight_current_line()
+        
+        # Setting placeholder text
+        self.setPlaceholderText("Escriba aquí...")
+
+    def line_number_area_width(self):
+        digits = 1
+        max_num = max(1, self.blockCount())
+        while max_num >= 10:
+            max_num //= 10
+            digits += 1
+        
+        space = 3 + self.fontMetrics().horizontalAdvance('9') * digits
+        return space
+
+    def update_line_number_area_width(self, _):
+        self.setViewportMargins(self.line_number_area_width(), 0, 0, 0)
+
+    def update_line_number_area(self, rect, dy):
+        if dy:
+            self.line_number_area.scroll(0, dy)
+        else:
+            self.line_number_area.update(0, rect.y(), self.line_number_area.width(), rect.height())
+        
+        if rect.contains(self.viewport().rect()):
+            self.update_line_number_area_width(0)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        cr = self.contentsRect()
+        self.line_number_area.setGeometry(QRect(cr.left(), cr.top(), self.line_number_area_width(), cr.height()))
+
+    def highlight_current_line(self):
+        extra_selections = []
+        
+        if not self.isReadOnly():
+            selection = QTextEdit.ExtraSelection()
+            line_color = QColor(Qt.GlobalColor.lightGray).lighter(130)
+            selection.format.setBackground(line_color)
+            selection.format.setProperty(QTextFormat.Property.FullWidthSelection, True)
+            selection.cursor = self.textCursor()
+            selection.cursor.clearSelection()
+            extra_selections.append(selection)
+        
+        self.setExtraSelections(extra_selections)
+
+    def lineNumberAreaPaintEvent(self, event):
+        painter = QPainter(self.line_number_area)
+        painter.fillRect(event.rect(), Qt.GlobalColor.white)
+        
+        block = self.firstVisibleBlock()
+        block_number = block.blockNumber()
+        top = round(self.blockBoundingGeometry(block).translated(self.contentOffset()).top())
+        bottom = top + round(self.blockBoundingRect(block).height())
+        
+        while block.isValid() and top <= event.rect().bottom():
+            if block.isVisible() and bottom >= event.rect().top():
+                number = str(block_number + 1)
+                painter.setPen(Qt.GlobalColor.darkGray)
+                painter.drawText(0, top, self.line_number_area.width(), self.fontMetrics().height(),
+                                Qt.AlignmentFlag.AlignRight, number)
+            
+            block = block.next()
+            top = bottom
+            bottom = top + round(self.blockBoundingRect(block).height())
+            block_number += 1
+
+    def toPlainText(self):
+        return super().toPlainText()
+
+    def setText(self, text):
+        super().setPlainText(text)
+
 
 class CompilerIDE(QMainWindow):
     def __init__(self):
@@ -45,8 +143,7 @@ class CompilerIDE(QMainWindow):
         self.statusBar().addWidget(self.status_label)
     
     def load_editor(self):
-        self.text_edit = QTextEdit()
-        self.text_edit.setPlaceholderText("Escriba aquí...")
+        self.text_edit = CodeEditor()  # Using our custom CodeEditor instead of QTextEdit
         
         self.tabs = QTabWidget()
         self.tabs.addTab(QTextEdit(), "Léxico")
